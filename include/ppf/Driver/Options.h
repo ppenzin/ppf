fac0e55cd76922600400f1ef24920df471327940
Petr Penzin <penzin.dev@gmail.com>
2018-04-08 11:32:26 -0700

    Add skeleton code for running the compiler
    Enable generating command-line options and add a dummy entry point to
    the program.

---
+++ include/ppf/Driver/Options.h
@@ -0,0 +1,52 @@
+//===--- Options.h - Option info & table ------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_PPF_DRIVER_OPTIONS_H
+#define LLVM_PPF_DRIVER_OPTIONS_H
+
+#include <memory>
+
+namespace llvm {
+namespace opt {
+class OptTable;
+}
+} // namespace llvm
+
+namespace ppf {
+namespace driver {
+
+namespace options {
+/// Flags specifically for ppf options.  Must not overlap with
+/// llvm::opt::DriverFlag.
+enum PPFFlags {
+  DriverOption = (1 << 4),
+  LinkerInput = (1 << 5),
+  NoArgumentUnused = (1 << 6),
+  Unsupported = (1 << 7),
+  PPF1Option = (1 << 10),
+  NoDriverOption = (1 << 12),
+  Ignored = (1 << 13)
+};
+
+enum ID {
+  OPT_INVALID = 0, // This is not an option ID.
+#define OPTION(PREFIX, NAME, ID, KIND, GROUP, ALIAS, ALIASARGS, FLAGS, PARAM,  \
+               HELPTEXT, METAVAR, VALUES)                                      \
+  OPT_##ID,
+#include "ppf/Driver/Options.inc"
+  LastOption
+#undef OPTION
+};
+} // namespace options
+
+std::unique_ptr<llvm::opt::OptTable> createDriverOptTable();
+} // namespace driver
+} // namespace ppf
+
+#endif
---
+++ tools/driver/main.cpp
@@ -0,0 +1,21 @@
+//===-- main.cpp - LLVM-based P4 compiler ---------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// The P4 (eBPF) Compiler.
+//
+//===----------------------------------------------------------------------===//
+
+#include "ppf/Driver/Options.h"
+#include<iostream>
+
+int
+main(void) {
+  std::cout << "Work in progress!" << std::endl;
+  return 0;
+}
